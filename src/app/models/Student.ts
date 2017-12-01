f71ac9ac397bdc2b4da17a739dbedd731e164f25
raghavan <raghavan.as@hotmail.co.in>
2017-12-01 20:49:49 +0530

    commit

---
+++ src/app/models/Student.ts
@@ -0,0 +1,8 @@
+export class Student {
+
+    name: string;
+    roll_number: number;
+    age: number;
+    gender: string;
+    isDayScholar: boolean;
+}
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/Student';
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  studentList: Student[];
+
+  constructor() {
+    this.studentList = [
+      {
+        name: 'Raghavan',
+        roll_number: 1261,
+        age: 18,
+        gender: 'male  ',
+        isDayScholar: true
+      },
+      {
+        name: 'Karthik ',
+        roll_number: 1262,
+        age: 19,
+        gender: 'male  ',
+        isDayScholar: true
+    },
+      {
+        name: 'Priyanka',
+        roll_number: 1263,
+        age: 19,
+        gender: 'female',
+        isDayScholar: false
+    },
+      {
+        name: 'Sabah   ',
+        roll_number: 1264,
+        age: 18,
+        gender: 'female',
+        isDayScholar: false
+    }
+    ];
+   }
+
+  ngOnInit() {
+  }
+
+}
